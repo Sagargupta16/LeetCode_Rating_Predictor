2f5123d7d61e36cfb73c03fcd5b16cad65d3180c
Sagar Gupta <sg85207@gmail.com>
2023-12-22 14:08:47 +0530

    Update PredictionComponent CSS and main.py

--- client/src/components/PredictionComponent.js
+++ client/src/components/PredictionComponent.js
@@ -2,56 +2,103 @@ import React, { useState } from 'react';
 import './PredictionComponent.css';
 
 const PredictionComponent = () => {
-    const [currentRating, setCurrentRating] = useState('');
+    const [username, setUsername] = useState('');
     const [contestRank, setContestRank] = useState('');
+    const [totalParticipants, setTotalParticipants] = useState('');
     const [predictionResult, setPredictionResult] = useState(null);
+    const [rating, setRating] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [warning, setWarning] = useState(''); // Add warning state
 
     const handleSubmit = async (event) => {
-      event.preventDefault();
-      try {
-          // Update the URL to point to the FastAPI server
-          const response = await fetch('http://localhost:8000/api/predict', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({ currentRating, contestRank }),
-          });
-  
-          if (!response.ok) {
-              throw new Error(`HTTP error! status: ${response.status}`);
-          }
-  
-          const data = await response.json();
-          setPredictionResult(data.changeInRating); // Assuming 'changeInRating' is the key in the response
-      } catch (error) {
-          console.error('There was a problem with the fetch operation:', error);
-      }
-  };
-  
+        event.preventDefault();
+        setIsLoading(true);
+        setWarning(''); // Clear previous warnings
+        setPredictionResult(null); // Reset predictionResult to null
+
+        try {
+            const rank = parseInt(contestRank);
+            const participants = parseInt(totalParticipants);
+
+            if (isNaN(rank) || isNaN(participants) || rank < 0 || rank > participants || participants > 50000) {
+                setWarning('Invalid input values. Rank should be between 0 and Total Participants, and Total Participants should not exceed 50,000.');
+            } else {
+                const response = await fetch('http://localhost:8000/api/predict', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        username: username,
+                        contestRank: rank,
+                        totalParticipants: participants,
+                    }),
+                });
+
+                if (!response.ok) {
+                    // Handle the case when the username does not exist
+                    if (response.status === 400) {
+                        setWarning('Username does not exist.');
+                    } else {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+                } else {
+                    const data = await response.json();
+                    setPredictionResult(data.changeInRating);
+                    setRating(data.rating);
+                }
+            }
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
     return (
         <div className="PredictionComponent">
             <h1>Leetcode Rating Predictor</h1>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className='prediction-form'>
                 <label>
-                    Current Rating:
+                    Enter Your Username
                     <input
-                        type="decimal"
-                        value={currentRating}
-                        onChange={(e) => setCurrentRating(e.target.value)}
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
                     />
                 </label>
                 <label>
-                    Contest Rank Achieved:
+                    Rank in Latest Contest
                     <input
                         type="number"
                         value={contestRank}
                         onChange={(e) => setContestRank(e.target.value)}
                     />
                 </label>
+                <label>
+                    Total Participants in Latest Contest
+                    <input
+                        type="number"
+                        value={totalParticipants}
+                        onChange={(e) => setTotalParticipants(e.target.value)}
+                    />
+                </label>
                 <button type="submit">Submit</button>
             </form>
-            {predictionResult !== null && <div className='prediction-output' >Change in rating is: {predictionResult}</div>}
+            <div className='prediction-info'>
+                {isLoading ? (
+                    <div className='loader'>Loading...</div>
+                ) : (
+                    predictionResult !== null && (
+                        <div className='prediction-output'>
+                            <p>Your Current Rating: {Math.round(rating)}</p>
+                            <p>Change in Rating: {Math.round(predictionResult)}</p>
+                            <p>Updated Rating: {Math.round(rating + predictionResult)}</p>
+                        </div>
+                    )
+                )}
+                {warning && <p className='warning'>{warning}</p>}
+            </div>
         </div>
     );
 };
--- main.py
+++ main.py
@@ -4,28 +4,47 @@ from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 import tensorflow as tf
 import joblib
+import requests
 
 # Load the trained model and the scaler
-model_path = './model.keras'
-scaler_path = './scaler.save'
+model = tf.keras.models.load_model('./model.keras')
+scaler = joblib.load('./scaler.save')
 
-try:
-    model = tf.keras.models.load_model(model_path)
-    scaler = joblib.load(scaler_path)
-except Exception as e:
-    print(f"Error loading model or scaler: {e}")
-    # Exit if model or scaler cannot be loaded
-    exit(1)
+# GraphQL query for fetching user contest data
+query = """
+query userContestRankingInfo($username: String!) {
+    userContestRanking(username: $username) {
+        attendedContestsCount
+        rating
+    }
+}
+"""
 
+# Headers for the GraphQL request
+headers = {"Content-Type": "application/json"}
+
+# Fetch data for a given username using GraphQL
+def fetch_data(username):
+    response = requests.post(
+        "https://leetcode.com/graphql",
+        headers=headers,
+        json={"query": query, "variables": {"username": username}}
+    )
+    if response.status_code == 200:
+        data = response.json().get("data", {}).get("userContestRanking", [])
+        if not data:
+            raise ValueError("Username does not exist")
+        return data
+    else:
+        print(f"Error fetching data for username {username}: {response.status_code}")
+        raise HTTPException(status_code=500, detail="Error fetching data from LeetCode API")
+
+# Normalize and make a prediction based on user input
 def make_prediction(input_data):
-    try:
-        input_scaled = scaler.transform(input_data)
-        input_scaled = input_scaled.reshape((input_scaled.shape[0], 1, input_scaled.shape[1]))
-        prediction = model.predict(input_scaled)
-        return prediction[0][0]
-    except Exception as e:
-        print(f"Error during prediction: {e}")
-        raise HTTPException(status_code=500, detail="Error making prediction")
+    input_scaled = scaler.transform(input_data)
+    input_scaled = input_scaled.reshape((input_scaled.shape[0], 1, input_scaled.shape[1]))
+    prediction = model.predict(input_scaled)
+    return float(prediction[0][0])  # Convert to standard Python float
 
 app = FastAPI()
 
@@ -39,16 +58,35 @@ app.add_middleware(
 )
 
 class PredictionInput(BaseModel):
-    currentRating: float
+    username: str
     contestRank: int
+    totalParticipants: int
 
 @app.post("/api/predict")
 async def predict(input_data: PredictionInput):
-    input1 = input_data.currentRating
-    input2 = input_data.contestRank
-    input3 = input1 / input2 if input2 != 0 else 0
+    try:
+        data = fetch_data(input_data.username)
+        if not data or "rating" not in data or "attendedContestsCount" not in data:
+            raise ValueError("Invalid data received from fetch_data")
+        
+        input1 = data["rating"]
+        input2 = input_data.contestRank
+        input3 = input_data.totalParticipants
+        input4 = (input2 * 100) / input3 if input3 != 0 else 0
+        input5 = data["attendedContestsCount"]
 
-    prediction = make_prediction(np.array([[input1, input2, input3]]))
+        prediction = make_prediction(np.array([[input1, input2, input3, input4, input5]]))
+        
+        output ={
+            "changeInRating": prediction,
+            "rating": input1,
+        }
+        
+        return output
 
-    return {"changeInRating": prediction}
+    except ValueError as ve:
+        # Handle the custom ValueError for username not existing
+        raise HTTPException(status_code=400, detail=str(ve))
 
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
